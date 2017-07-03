c1aff33b310fe13f0c8a6409d9e0a74e97b104fd
Sweet-Kiss <1196940127@qq.com>
2017-07-04 00:20:22 +0800

    详情页跳转

--- src/components/editDetail/editDetail.vue
+++ src/components/editDetail/editDetail.vue
@@ -1,9 +1,63 @@
 <template>
-  <div class="dd">编辑详情页</div>
+  <div class="Edit_w">
+  	 <div class="top_w"><span class="back"><img src="./back.png" alt=""></span>个人信息录入</div>
+  	 <div class="container">
+  	  <ul class="mess_list">
+  	 	<li><span>直线主管：</span>SL-20120200349刘丽</li>
+  	 	<li><span>RD区域总监：</span>RD-Alex.YANG</li>
+  	 	<li><span>AM：</span>AM-Alex.YANG</li>
+  	 	<li><span>所在营运点：</span>TS Lilly Research</li>
+  	 	<li><span>员工编码：</span>20111000679</li>
+  	 	<li><span>中文名：</span>夏鸣莹</li>
+  	 	<li><span>英文名：</span>20111000679</li>
+  	 	<li><span>所在地：</span>  SH</li>
+  	 	<li><span>细分市场/服务：</span>  COR</li>
+  	 	<li><span>本岗时间：</span>  2014/8/1</li>
+  	  </ul>
+  	 </div>
+ <div class="weui_cells weui_cells_access top_add">
+   <a class="weui_cell" href="javascript:;">
+       <div class="weui_cell_bd weui_cell_primary">
+            <p>语言：</p>
+        </div>
+        <div class="weui_cell_ft">
+        </div>
+    </a>
+</div>
+  </div>
 </template>
 
 <script>
 </script>
 <style>
-
+.top_w {
+	position: relative;
+	height: 50px;
+	line-height: 50px;
+	text-align: center;
+	color: #04be02;
+	border-bottom: #ccc solid 1px;
+}
+.back {
+	position: absolute;
+	display: inline-block;
+	top:8px;
+	left: 8px;
+	width: 25px;
+}
+.back img {
+	width: 100%;
+}
+.mess_list li {
+	height: 23px;
+	line-height: 23px;
+	color: #474646;
+}
+.mess_list li span {
+   color: #04be02;
+   margin-right: 10px;
+}
+.top_add {
+	margin-top: 10px;
+}
 </style>

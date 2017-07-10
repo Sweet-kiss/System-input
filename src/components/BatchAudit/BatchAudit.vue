f5775c2fd9c0911efca7c8d74642c704c79fcd46
Daheng <Daheng_Zhang@outlook.com>
2017-07-10 21:56:39 +0800

    批量请求

--- src/components/BatchAudit/BatchAudit.vue
+++ src/components/BatchAudit/BatchAudit.vue
@@ -13,7 +13,7 @@
       <mt-index-section v-for="(value, key) in list" :index="key" v-if="value.length>0">
         <div v-for="item in value" class="weui_cells weui_cells_access weui_add">
           <a class="weui_cell" href="javascript:;">
-            <p class="checkbox_RR"><input class="check_b"  style="width:25px;height:25px;margin-top:10px;background-color:#fff" name="Fruit" type="checkbox" value="" /></p>
+            <p class="checkbox_RR"><input class="check_b"  style="width:25px;height:25px;margin-top:10px;background-color:#fff" name="Fruit" type="checkbox" v-model="selecteds" :value="item.talentid" /></p>
              <div class="weui_cell_bd weui_cell_primary" @click="check(item)">
                <p class="code_m">{{ item.code }}</p>
                 <p class="name_o"><span>{{ item.name }}</span><span>{{ item.enName }}</span></p>
@@ -38,19 +38,22 @@ export default {
     return {
       informations: [],
       talent:[],
-       list: {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [], 'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [], 'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []}
+       list: {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [], 'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [], 'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []},
+       ids: [],
+       selecteds: []
     }
   },
   created () {
     let _this = this
     this.$http.get('http://121.40.75.24:8010/api/sodexo/getTalent?type=2').then((response) => {
-      // console.log(JSON.stringify(response.body.talent))
+      console.log(JSON.stringify(response.body))
       _this.talent = response.body.talent
       // console.log(response)
       // _this.talent = _this.talent.sort(this.compare('enName'))
       // console.log(JSON.stringify(_this.talent))
       for (var i=0; i<_this.talent.length; i++) {
         _this.list[_this.talent[i].enName.substring(0, 1).toUpperCase()].push(_this.talent[i])
+        _this.ids.push(_this.talent[i].talentid)
       }
       console.log(JSON.stringify(_this.list))
     })
@@ -60,15 +63,26 @@ export default {
       this.$router.push({ path: '/edit' })
    },
    selectAll () {
-       $(".check_b").attr("checked", true)
-       console.log($("#check_b").attr)
+    this.selecteds = this.ids
+       // $(".check_b").attr("checked", true)
+       // console.log($("#check_b").attr)
    },
     alert () {
-       Toast({
-          message: '提交成功！',
-          position: 'center',
-          duration: 5000
-       })
+      let result = []
+      for (let i=0; i<this.selecteds.length; i++) {
+        let tmp = {id: this.selecteds[i]}
+        result.push(tmp)
+      }
+      this.$http.post('http://121.40.75.24:8010/api/sodexo/updateTalent', {batch: 'true',comments: result}).then((response) => {
+        if (response.body) {
+          Toast({
+            message: '提交成功！',
+            position: 'center',
+            duration: 5000
+          })
+        }
+      }, (response) => {})
+       
     },
     check (item) {
       this.$router.push({ path: '/twoRemarks', query: { item: item } })
